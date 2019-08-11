6c22cad196b4c729bf53a0dc67042ed0f808dbfc
Hramtsov Sergey <serg.khram11@gmail.com>
2019-08-11 14:11:09 +0200

    fix

--- HW02/3.sql
+++ HW02/3.sql
@@ -8,11 +8,12 @@
 
 SELECT
     DISTINCT
-    DATENAME(MONTH,sord.OrderDate) AS OrderMonthName
+    sord.OrderID
+    ,sord.OrderDate
+    ,DATENAME(MONTH,sord.OrderDate) AS OrderMonthName
     ,DATEPART(qq,sord.OrderDate) AS OrderQuater
     ,CONVERT(INT, DATEPART(MONTH,sord.OrderDate)/4.1)+1 AS ThirdOfYear
-    ,sord.OrderID
-	,sord.OrderDate
+	
 FROM
     Sales.Orders AS sord JOIN Sales.OrderLines slines ON sord.OrderID = slines.OrderID
 WHERE  sord.OrderDate IS NOT NULL AND (slines.Quantity > 20 OR slines.UnitPrice>100)
@@ -23,11 +24,12 @@ GO
 
 SELECT
     DISTINCT
-    DATENAME(MONTH,sord.OrderDate) AS OrderMonthName
+    sord.OrderID
+	,sord.OrderDate
+    ,DATENAME(MONTH,sord.OrderDate) AS OrderMonthName
     ,DATEPART(qq,sord.OrderDate) AS OrderQuater
     ,CONVERT(INT, DATEPART(MONTH,sord.OrderDate)/4.1)+1 AS ThirdOfYear
-    ,sord.OrderID
-	,sord.OrderDate
+
 FROM
     Sales.Orders AS sord JOIN Sales.OrderLines slines ON sord.OrderID = slines.OrderID
 WHERE  sord.OrderDate IS NOT NULL AND (slines.Quantity > 20 OR slines.UnitPrice>100)
