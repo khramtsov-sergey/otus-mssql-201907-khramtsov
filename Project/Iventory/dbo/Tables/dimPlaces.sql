87a8dccca3cabaa440f7c9e7d5eaa4ddc0ab9299
Sergey Khramtsov <serg.khram11@gmail.com>
2019-12-10 11:57:48 +0100

    HW20

---
+++ Project/Iventory/dbo/Tables/dimPlaces.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[dimPlaces]
+(
+	[PlaceId] [int] PRIMARY KEY,
+			CONSTRAINT FK_dimPlaces_Items 
+			FOREIGN KEY (PlaceID) REFERENCES dbo.[dimPlaces] ([PlaceId]),
+	[EmployeeID] [int] NULL,
+	[OfficeID] [smallint] NULL,
+	[FirstName] [nvarchar](128) NULL,
+	[LastName] [nvarchar](128) NULL,
+	[Email] [nvarchar](128) NULL,
+	[CompanyID] [smallint]  NULL,
+	[OfficeName] [nvarchar](32) NULL,
+	[CompanyName] [nvarchar](128) NULL,
+)
--- Project/Seed_v2.ps1
+++ Project/Seed_v2.ps1
@@ -1,26 +1,130 @@
-﻿$User = "Admin"
-$Password = "AdminPassword"
-$start = Get-Date
+﻿$start = Get-Date
 
-$Companies = Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password -Database Iventory -QueryTimeout 600 -Query "
+$postDeployment = "
+USE [master]
+ALTER DATABASE Iventory
+SET ENABLE_BROKER; 
+
+ALTER DATABASE Iventory SET TRUSTWORTHY ON;
+
+ALTER AUTHORIZATION    
+   ON DATABASE::Iventory TO [sa];
+
+USE [Iventory]
+
+DECLARE @responseMessage NVARCHAR(250)	
+
+IF NOT EXISTS (SELECT 1 FROM dbo.DeviceStatuses)
+BEGIN
+INSERT INTO dbo.DeviceStatuses([StatusId],[Name],[Description])
+VALUES(1,'Not in use',''),(2,'In use',''),(3,'Decommissioned',''),(4,'In service',''),(5,'Remove','')
+END
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Roles)
+BEGIN
+INSERT INTO dbo.Roles([Name],[Description])
+VALUES ('Administrator','Can perform any activity'),('User','Limited permissions'),('Viewer','Read-only role')
+END
+
+IF NOT EXISTS (SELECT 1 FROM dbo.DeviceTypes)
+BEGIN
+EXEC	[dbo].[usp_AddDeviceType]
+		@pDeviceType = N'Laptop'
+		,@responseMessage = @responseMessage OUTPUT
+
+EXEC	[dbo].[usp_AddDeviceType]
+		@pDeviceType = N'PC'
+		,@responseMessage = @responseMessage OUTPUT
+
+EXEC	[dbo].[usp_AddDeviceType]
+		@pDeviceType = N'Printer'
+		,@responseMessage = @responseMessage OUTPUT
+END
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Companies)
+BEGIN
+EXEC	[dbo].[usp_AddCompany]
+		@pCompanyName = N'ACompany'
+		,@responseMessage = @responseMessage OUTPUT
+
+EXEC	[dbo].[usp_AddCompany]
+		@pCompanyName = N'BCompany'
+		,@responseMessage = @responseMessage OUTPUT
+
+EXEC	[dbo].[usp_AddCompany]
+		@pCompanyName = N'CCompany'
+		,@responseMessage = @responseMessage OUTPUT
+END
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Suppliers)
+BEGIN
+EXEC	[dbo].[usp_AddSupplier]
+		@pSupplierName = N'ASupplier'
+		,@responseMessage = @responseMessage OUTPUT
+
+EXEC	[dbo].[usp_AddSupplier]
+		@pSupplierName = N'BSupplier'
+		,@responseMessage = @responseMessage OUTPUT
+
+EXEC	[dbo].[usp_AddSupplier]
+		@pSupplierName = N'CSupplier'
+		,@responseMessage = @responseMessage OUTPUT
+END
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Contracts)
+BEGIN
+INSERT INTO dbo.Contracts([CompanyID],[SupplierID],[ContractNumber])
+VALUES	 (1,1,'NU-M-BER-11')
+		,(1,2,'NU-M-BER-12')
+		,(1,3,'NU-M-BER-13')
+		,(2,1,'NU-M-BER-21')
+		,(2,2,'NU-M-BER-22')
+		,(2,3,'NU-M-BER-23')
+		,(3,1,'NU-M-BER-31')
+END
+
+
+"
+
+Write-Host("PostDeployment")
+Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query $postDeployment
+
+# Add current username to Users
+$user = $env:UserDomain+'\'+$env:UserName
+$instance = ".\SQL2017"
+$DatabaseName = "Iventory"
+Write-Host("Add current user")
+Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Database Iventory -Query "
+DECLARE @responseMessage NVARCHAR(250)
+EXEC	dbo.usp_AddUser
+		 @pLoginName =	N'$user'
+		,@pPassword	=	N'-'
+		,@pFirstName =	N'YourNameHere'
+		,@pLastName =	N'YourLastNameHere'
+		,@pRoleID =		1
+		,@responseMessage = @responseMessage OUTPUT
+"
+
+$Companies = Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
 SELECT CompanyID FROM dbo.Companies
 " | Select -expand CompanyID
 
-$Types = Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password -Database Iventory -QueryTimeout 600 -Query "
+$Types = Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
 SELECT TypeID FROM dbo.DeviceTypes
 " | Select -expand TypeID
 
-$Statuses = Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password  -Database Iventory -QueryTimeout 600 -Query "
+$Statuses = Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
 SELECT StatusID FROM dbo.DeviceStatuses
 " | Select -expand StatusID
 
 # Add Offices
+Write-Host("Add Offices")
 foreach ($i in 1..32){
     $companyID = Get-Random -Minimum 1 -Maximum $Companies.Count
     $pCompanyID = $Companies[$companyID]
     $pName = "Office N"+$i
     $pAddress = "Address for Office N"+$i
-    Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password  -Database Iventory -QueryTimeout 600 -Query "
+    Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
     DECLARE @responseMessage NVARCHAR(250)
     EXEC [dbo].[usp_AddOffice]
                  @pName = '$pName'
@@ -30,16 +134,18 @@ foreach ($i in 1..32){
     "
 }
 
-$Offices = Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password -Database Iventory -QueryTimeout 600 -Query "
+$Offices = Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
 SELECT OfficeID, name FROM dbo.Offices
 "
+
+Write-Host("Add Warehouses")
 # Add Warehouses
 foreach ($i in 1..32){
     $officeID = Get-Random -Minimum 1 -Maximum $Offices.Count
     $pOfficeID = $Offices[$officeID].OfficeID
     $name = $Offices[$officeID].Name
     $pName = "Warehouse $i for Office $name "
-    Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password  -Database Iventory -QueryTimeout 600 -Query "
+    Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
     DECLARE @responseMessage NVARCHAR(250)
     EXEC [dbo].[usp_AddWarehouse]
                  @pName = '$pName'
@@ -49,36 +155,38 @@ foreach ($i in 1..32){
     "
 }
 
+Write-Host("Add employees")
 # Add Employees
 foreach($i in 1..30000){
     $companyID = Get-Random -Minimum 1 -Maximum $Companies.Count
     $pCompanyID = $Companies[$companyID]
     $pFirstName = "FirstName"+$i
     $pLastName = "LastName"+$i
-
-    Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password  -Database Iventory -QueryTimeout 600 -Query "
+    $pEmail = $pFirstName.SubString(0,1)+"."+$pLastName + "@company.com"
+    Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
     DECLARE @responseMessage NVARCHAR(250)
     EXEC [dbo].[usp_AddEmployee]
                  @pFirstName = '$pFirstName'
                 ,@pLastName = '$pLastName'
                 ,@pCompanyID = $companyID
+                ,@pEmail = '$pEmail'
                 ,@responseMessage = @responseMessage OUTPUT
     "
 }
 
-$Employees = Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password -Database Iventory -QueryTimeout 600 -Query "
+$Employees = Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
 SELECT EmployeeID FROM dbo.Employees
 " | Select -expand EmployeeID
 
+Write-Host("Add Places")
 # Add Places
-
 foreach ($i in 1..50000){
     $officeID = Get-Random -Minimum 1 -Maximum $Offices.Count
     $pOfficeID = $Offices[$officeID].OfficeID
     $employeeID = Get-Random -Minimum 1 -Maximum $Employees.Count
     $pEmployeeID = $Employees[$employeeID]
 
-    Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password  -Database Iventory -QueryTimeout 600 -Query "
+    Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
     DECLARE @responseMessage NVARCHAR(250)
     EXEC [dbo].[usp_AddPlace]
                  @pEmployeeID = $pEmployeeID
@@ -88,14 +196,15 @@ foreach ($i in 1..50000){
     "
 }
 
-$Warehouses = Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password  -Database Iventory -QueryTimeout 600 -Query "
+$Warehouses = Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
 SELECT WarehouseID FROM dbo.Warehouses
 " | Select -expand WarehouseID
 
-$Places = Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password  -Database Iventory -QueryTimeout 600 -Query "
+$Places = Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
 SELECT PlaceID FROM dbo.Places
 " | Select -expand PlaceID
 
+Write-Host("Add Items")
 foreach($i in 1..100000){
     $typeID = Get-Random -Minimum 1 -Maximum $Types.Count
     $pTypeID =  $Types[$typeID]
@@ -115,7 +224,7 @@ foreach($i in 1..100000){
     $statusID = Get-Random -Minimum 1 -Maximum $Statuses.Count
     $pStatusID = $Statuses[$statusID]
 
-    Invoke-Sqlcmd -ServerInstance ".\SQL2017" -Username $User -Password $Password  -Database Iventory -QueryTimeout 600 -Query "
+    Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
     DECLARE @responseMessage NVARCHAR(250)
     EXEC [dbo].[usp_AddItem]
              @pTypeID       =$pTypeID
@@ -129,6 +238,24 @@ foreach($i in 1..100000){
             ,@responseMessage = @responseMessage OUTPUT
     "
 }
+Write-Host("Add dimPlaces")
+    Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -QueryTimeout 1200 -Query "
+    INSERT INTO [dbo].[dimPlaces]
+       SELECT   p.PlaceId
+		,p.EmployeeID
+		,p.OfficeID
+		,e.FirstName
+		,e.LastName
+		,e.Email
+		,o.CompanyID
+		,o.Name AS OfficeName
+		,c.Name AS CompanyName
+		FROM [dbo].[Places] AS p 
+		JOIN [dbo].[Employees] AS e ON p.EmployeeID = e.EmployeeId
+		JOIN [dbo].[Offices] AS o ON p.OfficeID = o.OfficeId
+		JOIN [dbo].[Companies] AS c ON o.CompanyID = c.CompanyId
+
+    "
 $finish = Get-Date
 
 $finish-$start
